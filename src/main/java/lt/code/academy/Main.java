ed6a73586d016293fc3ef3c1f7bc19076f9c8b98
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-30 15:56:17 +0200

    CompanyRed

--- src/main/java/lt/code/academy/Main.java
+++ src/main/java/lt/code/academy/Main.java
@@ -1,5 +1,14 @@
-package lt.code.academy;public class Main {
+package lt.code.academy;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        GenerateService service = new GenerateService(objectMapper);
+        service.createRandCompaniesFile(120);
     }
 }
